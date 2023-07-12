1a730ffb530c6973944507805af620b59bd6ffa0
Wajahat Kanju <wajahat5ahmad@gmail.com>
2023-07-12 23:00:36 +0500

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Main File
+ * PHP version 8.0
+ *
+ * @category Theme
+ * @package  Phoneixa_Star
+ * @author   Wajahat   <wajahat5ahmad@gmail.com>
+ * @license  http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License
+ * @link     https://wajahatkanju.github.io
+ */
+
+ 
